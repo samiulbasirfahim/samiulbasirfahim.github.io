b64b5cb278327c77eb548713e5bee71000857295
Samiul Basir Fahim <samiulbasirfahim360@gmail.com>
2023-05-25 19:50:07 +0600

    drag animation

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,9 +1,35 @@
-import Component from "@/components/component";
+"use client"
+import React from "react"
+import Component from "../../components/component"
+import { AnimatePresence, motion } from "framer-motion"
+import { usePathname } from "next/navigation"
 
-export default function Contacts() {
+export default function Contact() {
+  const pathname = usePathname()
   return (
-    <Component>
-      <h1>Contacts page</h1>
-    </Component>
+    <AnimatePresence mode="sync">
+      <motion.div
+        drag="y"
+        dragConstraints={{ top: 0, bottom: 0 }}
+        dragElastic={2}
+      >
+        <motion.div
+          key={pathname}
+          transition={{
+            x: { type: "spring", stiffness: 300, damping: 30 },
+            opacity: { duration: 0.2 },
+          }}
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          dragElastic={2}
+        >
+          <div className="bg-gray-400 h-[100vh] w-[100vw]">
+            <Component>
+              <h1>Contact page</h1>
+            </Component>
+          </div>
+        </motion.div>
+      </motion.div>
+    </AnimatePresence>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,37 @@
 "use client"
 import React from "react"
 import Component from "../components/component"
+import { AnimatePresence, motion } from "framer-motion"
+import { usePathname } from "next/navigation"
 
 
 export default function App() {
-  return <Component >
-    <h1>Home page</h1>
-  </Component>
+  const pathname = usePathname()
+  return (
+    <AnimatePresence mode="sync">
+      <motion.div
+        drag="y"
+        dragConstraints={{ top: 0, bottom: 0 }}
+        dragElastic={2}
+      >
+        <motion.div
+          key={pathname}
+          transition={{
+            x: { type: "spring", stiffness: 300, damping: 30 },
+            opacity: { duration: 0.2 },
+          }}
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          dragElastic={2}
+          
+        >
+          <div className="bg-gray-400 h-[100vh] w-[100vw]">
+            <Component>
+              <h1>Home page</h1>
+            </Component>
+          </div>
+        </motion.div>
+      </motion.div>
+    </AnimatePresence>
+  )
 }
